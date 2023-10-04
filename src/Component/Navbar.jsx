d29cb4cd1f8eff4411edb862c12d0c1e0d2d3d5b
Betelhem-Belete <betelhembelete0@gmail.com>
2023-10-04 15:07:21 -0400

    some components

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="d-flex justify-content-center bg-secondary text-light p-2">
+      <h2>crud app with rest api</h2>
+    </div>
+  );
+};
+
+export default Header;
