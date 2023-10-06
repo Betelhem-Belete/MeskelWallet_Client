692c75752849db35d5a477112770eeff94acd2ba
Betelhem-Belete <betelhembelete0@gmail.com>
2023-10-06 15:27:08 -0400

    debug the code bro

---
+++ src/Hooks/useAuthContext.jsx
@@ -0,0 +1,10 @@
+import { AuthContext } from '../Context/UserContext';
+import { useContext } from 'react';
+
+export const UseAuthContext = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw Error('usercontext must use inside a contextProvider');
+  }
+  return context;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { AuthContextProvider } from './Contexts/UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
