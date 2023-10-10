51edec99ec1c6eeefe8df98745e3d3ecbbd7c60e
Betelhem-Belete <betelhembelete0@gmail.com>
2023-10-10 01:38:10 -0400

    deplo

--- src/Hooks/useLogin.jsx
+++ src/Hooks/useLogin.jsx
@@ -5,7 +5,7 @@ export const useLogin = () => {
   const { dispatch } = UseAuthContext();
   const [isLoading, setIsLoading] = useState(null);
   const [error, setError] = useState(null);
-  const Toast = useToast();
+  // const Toast = useToast();
 
   const Apilogin = import.meta.env.VITE_API_LOGIN;
   const login = async (Email, password) => {
@@ -22,25 +22,11 @@ export const useLogin = () => {
     if (!response.ok) {
       setError(json.error);
       setIsLoading(false);
-      Toast({
-        title: 'Loged in failed',
-        status: 'error',
-        duration: 5000,
-        isClosable: true,
-        position: 'bottom',
-      });
     }
     if (response.ok) {
       dispatch({ type: 'LOGIN', payload: json });
       setIsLoading(false);
       localStorage.setItem('user', JSON.stringify(json));
-      Toast({
-        title: 'Loged in successfully',
-        status: 'success',
-        duration: 5000,
-        isClosable: true,
-        position: 'bottom',
-      });
     }
   };
   return { isLoading, error, login };
